89c7e73191cba32602f16b9a73c028579903ab26
Matthieu Dumont <jerska@live.fr>
2015-12-10 15:28:34 +0100

    chore(release): Add callback to ghpages.publish

--- scripts/publish-docs.js
+++ scripts/publish-docs.js
@@ -3,4 +3,9 @@ var ghpages = require('gh-pages');
 
 ghpages.publish(path.join(__dirname, 'docs', '_site'), {
   repo: 'git@github.com/algolia/zendesk'
+}, function (err) {
+  if (err) {
+    console.log("Error while pushing docs", err);
+  }
+  console.log("Pushed the gh-pages");
 });
