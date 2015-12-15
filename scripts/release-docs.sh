60c5d6dd6432a1abab0b0b496e1e31d94bc6a6fb
Sylvain UTARD <sylvain.utard@gmail.com>
2015-12-15 20:06:59 +0100

    Release: run the documentation.md generation before publication

--- scripts/release-docs.sh
+++ scripts/release-docs.sh
@@ -2,15 +2,15 @@
 
 version=`cat package.json | json version`
 
+npm run docs
+git add docs
+git commit docs -m "docs(release): v$version"
+git push
+
 cd docs
 bundle install
 rm -rf _site
 JEKYLL_ENV=production VERSION=$version bundle exec jekyll build
 cd ..
-
-npm run docs
-git add docs
-git commit docs -m "docs(release): v$version"
-git push
 
 node ./scripts/publish-docs.js
