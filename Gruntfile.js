e8c54df1d4387dea924650c52bf6d5f6b97e9dea
Matthieu Dumont <jerska@live.fr>
2015-12-23 15:58:29 +0100

    chore(grunt): Extract the documentation building in its own file

--- Gruntfile.js
+++ Gruntfile.js
@@ -138,46 +138,12 @@ module.exports = function (grunt) {
   grunt.registerTask('build', ['clean', 'build:js', 'build:css']);
   grunt.registerTask('build:js', ['webpack', 'sed:version', 'usebanner', 'uglify']);
   grunt.registerTask('build:css', ['concat:css', 'cssmin']);
+  grunt.registerTask('build:docs', 'build:docs:documentation.md');
   grunt.registerTask('server', 'connect:server');
   grunt.registerTask('lint', 'eslint');
   grunt.registerTask('dev', 'concurrent:dev');
-  grunt.registerTask('docs', 'docs:documentation.md');
 
-  grunt.registerTask('docs:documentation.md', 'Retrieve the documentation part of the README and hosts it on the website.', function () {
-    var done = this.async();
-
-    var fs = require('fs');
-
-    var readHeader = new Promise(function (resolve, reject) {
-      fs.readFile('./docs/headers/documentation.yml', function (err, data) {
-        if (err) return reject(err);
-        resolve(data.toString());
-      });
-    });
-
-    var readReadme = new Promise(function (resolve, reject) {
-      fs.readFile('./README.md', function (err, data) {
-        if (err) return reject(err);
-        var arr = data.toString().split('\n');
-        var keep = [];
-        var keeping = false;
-        for (var i = 0; i < arr.length; ++i) {
-          var line = arr[i];
-          if (keeping && line.match(/^##[^#]/)) break;
-          if (keeping) keep.push(line);
-          if (!keeping && line.match(/^## Documentation$/)) keeping = true;
-        }
-        resolve(keep.join('\n'));
-      });
-    });
-
-    Promise.all([readHeader, readReadme]).then(function (data) {
-      fs.writeFile('docs/documentation.md', data.join('\n'), function (err) {
-        if (err) throw err;
-        done();
-      });
-    });
-  });
+  grunt.registerTask('build:docs:documentation.md', require('./grunt/build/docs/documentation.md.js'));
 
   // load tasks
   // ----------
---
+++ grunt/build/docs/documentation.md.js
@@ -0,0 +1,37 @@
+/* eslint algolia/no-module-exports: 0, no-var: 0 */
+
+module.exports = function () {
+  var done = this.async();
+
+  var fs = require('fs');
+
+  var readHeader = new Promise(function (resolve, reject) {
+    fs.readFile('./docs/headers/documentation.yml', function (err, data) {
+      if (err) return reject(err);
+      resolve(data.toString());
+    });
+  });
+
+  var readReadme = new Promise(function (resolve, reject) {
+    fs.readFile('./README.md', function (err, data) {
+      if (err) return reject(err);
+      var arr = data.toString().split('\n');
+      var keep = [];
+      var keeping = false;
+      for (var i = 0; i < arr.length; ++i) {
+        var line = arr[i];
+        if (keeping && line.match(/^##[^#]/)) break;
+        if (keeping) keep.push(line);
+        if (!keeping && line.match(/^## Documentation$/)) keeping = true;
+      }
+      resolve(keep.join('\n'));
+    });
+  });
+
+  Promise.all([readHeader, readReadme]).then(function (data) {
+    fs.writeFile('docs/documentation.md', data.join('\n'), function (err) {
+      if (err) throw err;
+      done();
+    });
+  });
+};
