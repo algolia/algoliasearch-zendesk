2d753a368038a19e90b8aed36bb142a696ab6eaf
Matthieu Dumont <jerska@live.fr>
2016-01-26 20:43:40 +0100

    test(autocomplete.js): Fix jQuery issue with jsdom

--- app/test/AlgoliaSearchZendeskHC.js
+++ app/test/AlgoliaSearchZendeskHC.js
@@ -1,7 +1,26 @@
 /* eslint-env mocha */
 
-import expect from'expect';
+import jsdom from 'jsdom-global';
 
-it('should pass', () => {
-  expect(true).toBe(true);
+import expect from 'expect';
+
+describe('AlgoliasearchZendeskHC', () => {
+  let AlgoliasearchZendeskHC;
+
+  before(function () {
+    this.jsdom = jsdom();
+    AlgoliasearchZendeskHC = require('../src/AlgoliasearchZendeskHC.js').default;
+  });
+
+  it('should exist', () => {
+    expect(AlgoliasearchZendeskHC).toNotBe(undefined);
+  });
+
+  it('should throw usage without options', () => {
+    expect(() => (new AlgoliasearchZendeskHC())).toThrow(/Usage/);
+  });
+
+  after(function () {
+    this.jsdom();
+  });
 });
