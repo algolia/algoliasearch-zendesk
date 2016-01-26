b31f661133533d6f6266435cd9008541bd2240e3
Matthieu Dumont <jerska@live.fr>
2016-01-26 20:44:24 +0100

    chore(test): Use mochawesome reporter for mocha

--- app/gulp/test.js
+++ app/gulp/test.js
@@ -14,7 +14,7 @@ export default function (cb) {
   ).pipe(istanbul.hookRequire())
     .on('finish', function () {
       gulp.src(['test/**/*.js'])
-       .pipe(mocha())
+       .pipe(mocha({reporter: 'mochawesome'}))
        .pipe(istanbul.writeReports()) // Creating the reports after tests ran
 //       .pipe(istanbul.enforceThresholds({thresholds: {global: 90}})) // Enforce a coverage of at least 90%
        .on('end', cb);
