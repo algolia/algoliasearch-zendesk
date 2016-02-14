e12311c8e7e153241c6b996123f0c825b7ff8eaf
Matthieu Dumont <jerska@live.fr>
2016-02-14 16:31:03 +0100

    feat(fargs): options checking with fargs

--- app/src/AlgoliasearchZendeskHC.js
+++ app/src/AlgoliasearchZendeskHC.js
@@ -1,5 +1,7 @@
 import $ from './jQuery.js';
 
+import fargs from 'fargs';
+
 import defaultsDeep from 'lodash/defaultsDeep';
 import every from 'lodash/every';
 import isString from 'lodash/isString';
@@ -8,40 +10,37 @@ import loadTranslations from './translations.js';
 import autocomplete from './autocomplete.js';
 import instantsearch from './instantsearch.js';
 
-const usage = `Usage:
-algoliasearchZendeskHC({
-  applicationId,
-  apiKey,
-  subdomain,
-  [ indexPrefix ],
-  [ autocomplete ],
-  [ instantsearch ]
-})
-`;
-
-const defaultOptions = {
-  autocomplete: {
-    enabled: true,
-    inputSelector: '#query',
-    hitsPerPage: 5
-  },
-  baseUrl: '/hc/',
-  colors: {
-    primary: '#D4D4D4',
-    secondary: '#D4D4D4'
-  },
-  indexPrefix: 'zendesk_',
-  instantsearch: {
-    enabled: true,
-    tagsLimit: 15
-  },
-  poweredBy: true,
-  translations: {}
+function hitsPerPageValidator(val) {
+  return (val >= 1 && val <= 20) || 'should be between 1 and 20';
 };
+const optionsStructure = {required: true, type: 'Object', children: {
+  applicationId: {type: 'string', required: true},
+  apiKey: {type: 'string', required: true},
+  autocomplete: {type: 'Object', value: {}, children: {
+    enabled: {type: 'boolean', value: true},
+    inputSelector: {type: 'string', value: '#query'},
+    hitsPerPage: {type: 'number', value: 5, validators: [hitsPerPageValidator]}
+  }},
+  baseUrl: {type: 'string', value: '/hc/'},
+  colors: {type: 'Object', value: {}, children: {
+    primary: {type: 'string', value: '#D4D4D4'},
+    secondary: {type: 'string', value: '#D4D4D4'}
+  }},
+  indexPrefix: {type: 'string', value: 'zendesk_'},
+  instantsearch: {type: 'Object', value: {}, children: {
+    enabled: {type: 'boolean', value: true},
+    tagsLimit: {type: 'number', value: 15}
+  }},
+  poweredBy: {type: 'boolean', value: true},
+  subdomain: {type: 'string', required: true},
+  translations: {type: 'Object', value: {}}
+}};
+
 class AlgoliasearchZendeskHC {
-  constructor(options = {}) {
-    this._checkOptions(options);
-    options = defaultsDeep({}, options, defaultOptions);
+  constructor() {
+    let options = fargs().check('algoliasearchZendeskHC')
+      .arg('options', optionsStructure)
+      .values(arguments)[0];
 
     // once the DOM is initialized
     $(document).ready(() => {
@@ -55,12 +54,6 @@ class AlgoliasearchZendeskHC {
         this.autocomplete = instantsearch(options);
       }
     });
-  }
-
-  _checkOptions({applicationId, apiKey, subdomain}) {
-    const valuesToCheck = [applicationId, apiKey, subdomain];
-    const valid = every(valuesToCheck, (v) => isString(v));
-    if (!valid) throw new Error(usage);
   }
 }
 
