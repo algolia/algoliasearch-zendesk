555a47757cfd2658fc3f06ece3827d5c14bd5d11
Matthieu Dumont <jerska@live.fr>
2016-03-08 15:34:11 +0100

    chore(translations): use the browserify-shimmed I18n

--- app/src/translations.js
+++ app/src/translations.js
@@ -1,10 +1,8 @@
-/* global I18n */
-
 import isString from 'lodash/isString';
 import isUndefined from 'lodash/isUndefined';
 import isPlainObject from 'lodash/isPlainObject';
 
-function addTranslation(translations, key, fallback, I18nKey) {
+function addTranslation(I18n, translations, key, fallback, I18nKey) {
   if (isString(translations[key])) {
     return;
   }
@@ -24,19 +22,21 @@ function addTranslation(translations, key, fallback, I18nKey) {
 
 export function loadTranslations(options) {
   let ts = options.translations;
-  addTranslation(ts, 'article', 'Article', 'txt.help_center.views.admin.manage_knowledge_base.table.article');
-  addTranslation(ts, 'articles', 'Articles', 'txt.help_center.javascripts.arrange_content.articles');
-  addTranslation(ts, 'categories', 'Categories', 'txt.help_center.javascripts.arrange_content.categories');
-  addTranslation(ts, 'sections', 'Sections', 'txt.help_center.javascripts.arrange_content.sections');
-  addTranslation(ts, 'tags', 'Tags');
-  addTranslation(ts, 'search_by', 'Search by');
-  addTranslation(ts, 'no_result', 'No result');
-  addTranslation(ts, 'result', 'Result');
-  addTranslation(ts, 'results', 'Results');
-  addTranslation(ts, 'found_in', 'Found in');
-  addTranslation(ts, 'search_by', 'Search by');
-  addTranslation(ts, 'placeholder_autocomplete', 'Search in sections and articles');
-  addTranslation(ts, 'placeholder_instantsearch', 'Search in articles');
+  // Requiring it here because window.I18n isn't defined in the Document Head template
+  let I18n = require('I18n');
+  addTranslation(I18n, ts, 'article', 'Article', 'txt.help_center.views.admin.manage_knowledge_base.table.article');
+  addTranslation(I18n, ts, 'articles', 'Articles', 'txt.help_center.javascripts.arrange_content.articles');
+  addTranslation(I18n, ts, 'categories', 'Categories', 'txt.help_center.javascripts.arrange_content.categories');
+  addTranslation(I18n, ts, 'sections', 'Sections', 'txt.help_center.javascripts.arrange_content.sections');
+  addTranslation(I18n, ts, 'tags', 'Tags');
+  addTranslation(I18n, ts, 'search_by', 'Search by');
+  addTranslation(I18n, ts, 'no_result', 'No result');
+  addTranslation(I18n, ts, 'result', 'Result');
+  addTranslation(I18n, ts, 'results', 'Results');
+  addTranslation(I18n, ts, 'found_in', 'Found in');
+  addTranslation(I18n, ts, 'search_by', 'Search by');
+  addTranslation(I18n, ts, 'placeholder_autocomplete', 'Search in sections and articles');
+  addTranslation(I18n, ts, 'placeholder_instantsearch', 'Search in articles');
 }
 
 export default loadTranslations;
