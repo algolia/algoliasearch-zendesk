9f99ff1003fc1f246275476ac42a0a6f9d5f4172
Matthieu Dumont <jerska@live.fr>
2016-05-19 15:20:46 +0200

    docs(documentation): add anchors to titles

---
+++ docs/js/anchors.js
@@ -0,0 +1,10 @@
+var titles = document.querySelectorAll('.page-documentation h2[id]');
+
+for (var i = 0; i < titles.length; ++i) {
+  var elt = titles[i];
+  var anchor = document.createElement('a');
+  anchor.href = '#' + elt.id;
+  anchor.className = 'anchor';
+  anchor.innerText = ' # ';
+  elt.insertBefore(anchor, elt.firstChild);
+}
