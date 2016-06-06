38f65a1303ab795e9a0b00c36c9507f98acb6c9c
Matthieu Dumont <jerska@live.fr>
2016-06-06 19:00:18 +0200

    docs(showcases): make them great

--- docs/js/gifs.js
+++ docs/js/gifs.js
@@ -3,9 +3,8 @@ function responsiveImages () {
   for (var i = 0; i < imgs.length; ++i) {
     var img = imgs[i];
     var cs = window.getComputedStyle(img);
-    var width = parseInt(cs.width);
-    var height = parseInt(cs.height);
-    img.src = img.getAttribute('data-src').replace(/image\/upload/, "image/upload/w_" + width + ",h_" + height + ",c_fill")
+    var width = parseInt(cs.width) * (window.devicePixelRatio || 1);
+    img.src = img.getAttribute('data-src').replace(/image\/upload/, "image/upload/w_" + width + ",c_fill")
   }
 }
 
@@ -15,13 +14,16 @@ window.addEventListener('resize', function(event){
 
 responsiveImages();
 
-var gifs = document.querySelectorAll('a.gifs img');
+var gifs = document.querySelectorAll('.showcase.gif');
 for (var i = 0; i < gifs.length; i++) {
-  gifs[i].addEventListener('mouseover', function () {
-    this.src = this.src.replace('.png', '.gif')
-  });
+  (function (gif, mainImg) {
+    gif.addEventListener('mouseenter', function () {
+      mainImg.setAttribute('data-old-src', mainImg.src);
+      mainImg.src = gif.querySelector('img.animated-image').src;
+    });
 
-  gifs[i].addEventListener('mouseleave', function () {
-    this.src = this.src.replace('.gif', '.png')
-  });
+    gif.addEventListener('mouseleave', function () {
+      mainImg.src = mainImg.getAttribute('data-old-src');
+    });
+  }(gifs[i], gifs[i].querySelector('img.main-image')));
 }
