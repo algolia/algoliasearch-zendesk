1967ed2f47176af0d31d45c18cde5ff733ee9709
Matthieu Dumont <jerska@live.fr>
2016-06-06 18:00:57 +0200

    docs(gifs): fix image rendering by letting cloudinary

--- docs/js/gifs.js
+++ docs/js/gifs.js
@@ -1,12 +1,27 @@
+function responsiveImages () {
+  var imgs = document.querySelectorAll('img.img-responsive');
+  for (var i = 0; i < imgs.length; ++i) {
+    var img = imgs[i];
+    var cs = window.getComputedStyle(img);
+    var width = parseInt(cs.width);
+    var height = parseInt(cs.height);
+    img.src = img.getAttribute('data-src').replace(/image\/upload/, "image/upload/w_" + width + ",h_" + height + ",c_fill")
+  }
+}
+
+window.addEventListener('resize', function(event){
+  responsiveImages();
+});
+
+responsiveImages();
+
 var gifs = document.querySelectorAll('a.gifs img');
-
 for (var i = 0; i < gifs.length; i++) {
+  gifs[i].addEventListener('mouseover', function () {
+    this.src = this.src.replace('.png', '.gif')
+  });
 
-	gifs[i].addEventListener('mouseover', function(){
-		this.src = this.src.replace('.png', '.gif')
-	})
-
-	gifs[i].addEventListener('mouseleave', function(){
-		this.src = this.src.replace('.gif', '.png')
-	})
+  gifs[i].addEventListener('mouseleave', function () {
+    this.src = this.src.replace('.gif', '.png')
+  });
 }
