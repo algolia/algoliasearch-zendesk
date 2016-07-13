6015d40d7f5ae4538710fa4a4b70f563bcc100b5
Matthieu Dumont <Jerskouille@users.noreply.github.com>
2016-07-13 14:31:38 +0200

    feat(crawler): index while crawling (#56)

--- crawler/item.rb
+++ crawler/item.rb
@@ -12,29 +12,61 @@ module Zendesk
     attr_accessor :data
     attr_accessor :zendesk_obj
 
-    def self.singular
-      name.split('::').last.downcase
-    end
+    class << self
+      def singular
+        name.split('::').last.downcase
+      end
 
-    def self.plural
-      "#{singular}s".to_sym
-    end
+      def plural
+        "#{singular}s".to_sym
+      end
 
-    def self.index_name
-      "#{CONFIG['algolia_index_prefix']}#{CONFIG['app_name']}_#{plural}"
-    end
+      def index_name
+        "#{CONFIG['algolia_index_prefix']}#{CONFIG['app_name']}_#{plural}"
+      end
 
-    def self.index_settings
-      DEFAULT_INDEX_SETTINGS.merge(self::INDEX_SETTINGS)
-    end
+      def index_settings
+        DEFAULT_INDEX_SETTINGS.merge(self::INDEX_SETTINGS)
+      end
 
-    def self.index items
-      to_index = items.map(&:to_index).flatten
-      idx = Algolia::Index.new("#{index_name}.tmp")
-      idx.set_settings index_settings
-      to_index.each_slice(1000).each { |sub| idx.save_objects! sub }
-      Algolia.move_index "#{index_name}.tmp", index_name
-      puts "Indexed #{to_index.count} #{plural}"
+      def start_indexing
+        target_index.set_settings index_settings
+      end
+
+      def index items
+        return if items.empty?
+        to_index = items.map(&:to_index).flatten
+        to_index.each_slice(1000).each { |sub| target_index.save_objects! sub }
+        puts "Indexed #{to_index.count} #{plural}"
+      end
+
+      def finish_indexing
+        from, to = target_index.name, index_name
+        return if from == to
+        Algolia.move_index from, to
+      end
+
+      private
+
+      def target_index
+        return @target_index unless @target_index.nil?
+        Algolia::Index.new(index_name).get_settings
+      rescue => e
+        return @target_index = Algolia::Index.new(index_name) if e.code == 404
+        raise
+      else
+        @target_index = Algolia::Index.new("#{index_name}.tmp")
+      end
+
+      def first_indexing?
+        return @first_indexing unless @first_indexing.nil?
+        Algolia::Index.new(index_name).get_settings
+      rescue => e
+        return @first_indexing = true if e.code == 404 && e.message =~ 'Index does not exist'
+        raise
+      else
+        return false
+      end
     end
 
     def initialize crawler, obj # Or object id, see fetch
