19962ed957dbfb1ab260b52dc618f23b2e48fa1c
Matthieu Dumont <jerska@live.fr>
2016-07-25 15:32:35 +0200

    fix(translations): correct english placeholder
    Fixes #57

--- app/src/translations.js
+++ app/src/translations.js
@@ -463,7 +463,7 @@ const TRANSLATIONS = {
     da: 'Søg i vores artikler',
     de: 'In unseren Artikeln suchen',
     el: 'Κάντε αναζήτηση στα άρθρα μας',
-    en: 'Search in our articles',
+    en: 'Search our articles',
     es: 'Buscar en nuestros artículos',
     fi: 'Etsi artikkeleista',
     fr: 'Recherchez dans nos articles',
