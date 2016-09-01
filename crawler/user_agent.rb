9508d8df06a486252a9f3a96884aeb4b7633ff6e
Matthieu Dumont <jerska@live.fr>
2016-09-01 16:12:25 +0200

    feat(crawler): update to new user-agent format

---
+++ crawler/user_agent.rb
@@ -0,0 +1,25 @@
+require 'algoliasearch'
+
+class UserAgent
+  class Version
+    def self.crawler
+      File.read('VERSION').strip
+    end
+
+    def self.client
+      Algolia::VERSION
+    end
+
+    def self.ruby
+      RUBY_VERSION
+    end
+  end
+
+  def self.to_s
+    [
+      "Zendesk Integration (#{Version.crawler})",
+      "Algolia for Ruby (#{Version.client})",
+      "Ruby (#{Version.ruby})"
+    ].join('; ')
+  end
+end
