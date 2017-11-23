6ea44703a6acc10d8d27b5f1ba95780a40e5ed67
Matthieu Dumont <jerska@live.fr>
2017-11-23 16:49:32 +0100

    chore(scripts): try to make otp work

--- scripts/release-app.sh
+++ scripts/release-app.sh
@@ -15,16 +15,17 @@ read -p "[App] We'll \`npm publish\` with \"v$ALGOLIASEARCH_ZENDESK_VERSION\". C
 echo
 [[ $REPLY =~ ^[Yy]$ ]] || exit -1
 
-echo "[App] One time password: "
-read line
-[[ $line =~ [0-9]{6} ]] || exit -1
-OTP=$line
-
 # Build and publish app
 cd app/
 # No git-tag-version also disables the commit (See https://github.com/npm/npm/issues/7186)
 npm version --no-git-tag-version $ALGOLIASEARCH_ZENDESK_VERSION
 npm run clean
 NODE_ENV=production npm run build
+
+echo "[App] One time password: "
+read line
+[[ $line =~ [0-9]{6} ]] || exit -1
+OTP=$line
 npm publish --otp=$OTP
+
 cd ../
