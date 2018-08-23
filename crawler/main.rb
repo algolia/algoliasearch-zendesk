9e6919758aafa72bb790272f52a6e79e35415b22
Matthieu Dumont <jerska@live.fr>
2018-08-23 12:19:45 +0200

    chore(crawler): add error handling with IP output

--- crawler/main.rb
+++ crawler/main.rb
@@ -2,6 +2,17 @@
 
 require_relative './crawler.rb'
 
-@crawler = Crawler.new
-@crawler.crawl_and_index Zendesk::Article
-@crawler.crawl_and_index Zendesk::Post
+begin
+  @crawler = Crawler.new
+  @crawler.crawl_and_index Zendesk::Article
+  @crawler.crawl_and_index Zendesk::Post
+rescue => e
+  puts 'An error happened while crawling'
+  puts 'Message'
+  puts e.message
+  puts 'Additional information'
+  curl_or_wget = `if hash curl 2>/dev/null; then echo "curl -s"; elif hash wget 2>/dev/null; then echo "wget -qO-"; fi`.strip
+  puts "IP (should be whitelisted): #{`#{curl_or_wget} ipinfo.io/ip`}".strip
+  puts 'Stacktrace'
+  puts e.backtrace
+end
