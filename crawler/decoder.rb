88b24d6b9653cac2be043d2ee7b7bad2d246bc50
Sylvain UTARD <sylvain.utard@gmail.com>
2019-07-31 14:58:04 +0200

    Avoid using monkey patched version of the htmlentities gem

--- crawler/decoder.rb
+++ crawler/decoder.rb
@@ -1,8 +1,11 @@
 require 'htmlentities'
 
 class ZendeskIntegration::V2::Decoder < HTMLEntities
+  # work-around https://github.com/threedaymonk/htmlentities/issues/18
+  # to skip the decoding of `&lt;` and `&gt;`
   def decode input
-    super input, exclude: ['<', '>']
+    super(input.gsub('&lt;', '___LT___').gsub('&gt;', '___GT___')).gsub('___LT___', '&lt;').gsub('___GT___', '&gt;')
   end
 end
+
 ZendeskIntegration::V2::DECODER = ZendeskIntegration::V2::Decoder.new
