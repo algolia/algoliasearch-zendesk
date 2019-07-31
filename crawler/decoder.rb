f23b9f24004cddd12c00a9ae80f0830acbe373de
Matthieu Dumont <jerska@live.fr>
2019-07-31 15:11:47 +0200

    chore: add alternative decoder version

--- crawler/decoder.rb
+++ crawler/decoder.rb
@@ -4,7 +4,7 @@ class ZendeskIntegration::V1::Decoder < HTMLEntities
   # work-around https://github.com/threedaymonk/htmlentities/issues/18
   # to skip the decoding of `&lt;` and `&gt;`
   def decode input
-    super(input.gsub('&lt;', '___LT___').gsub('&gt;', '___GT___')).gsub('___LT___', '&lt;').gsub('___GT___', '&gt;')
+    super(input).gsub('<', '&lt;').gsub('>', '&gt;')
   end
 end
 ZendeskIntegration::V1::DECODER = ZendeskIntegration::V1::Decoder.new
