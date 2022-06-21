50e60a07d7040cbc8f3969e2a3ce75e045b51e7a
Sylvain Bellone <sylvain.bellone@algolia.com>
2022-06-21 15:42:30 +0200

    add TODO

--- app/gulp/lint.js
+++ app/gulp/lint.js
@@ -1,6 +1,8 @@
 import gulp from 'gulp';
 import eslint from 'gulp-eslint';
 
+// eslint-disable-next-line no-warning-comments
+// TODO we are now calling eslint directly, to remove entirely
 export default function () {
   return gulp
     .src(['index.js', 'src/**/*.js'])
